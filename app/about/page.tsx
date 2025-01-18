d003537733e92bb1cabd1af25284584080338e7f
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2025-01-18 15:19:37 +0100

    Update project metadata

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">About</h1>
+      <p className="text-lg text-gray-500">
+        This is a simple website built with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
